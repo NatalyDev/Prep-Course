23e7e9710fcb62c099510e77382dee32618a4ffc
NatalyDev <natalybusiness19@gmail.com>
2022-09-22 18:02:38 -0400

    Tarea en Marcha :)

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,15 @@ function crearGato (nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+
+  var gato = {
+    nombre : nombre,
+    edad : edad,
+    meow: function() {
+      return 'Meow!';
+    }
+  };
+  return gato;
 }
 
 function agregarPropiedad (objeto, property) {
@@ -13,6 +22,9 @@ function agregarPropiedad (objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+
+objeto [property] = null; //Agregamos una propiedad al objeto, esto lo podemos hacer con punto(.) o con corchetes []
+return objeto; //Devolvemos el objeto
 }
 
 function invocarMetodo (objeto, metodo) {
@@ -20,12 +32,26 @@ function invocarMetodo (objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+
+  objeto[metodo](); //Invocamos con los paréntesis
+
 }
 
 function multiplicarNumeroDesconocidoPorCinco (objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
+
+/* NAT :) Tomememos esyo como ejemplo
+var objetoMisterioso = {
+  numeroMisterioso:4;
+}
+*/
+
+//Creamos la variable resultado en donde vamos a guardar la multiplicación
+var resultado = objetoMisterioso.numeroMisterioso * 5; //Nat :) Tambien se puede colocar así: objetoMisterioso["numeroMisterioso"]*5;
+
+return resultado;
 
 }
 
@@ -34,19 +60,47 @@ function eliminarPropiedad (objeto, unaPropiedad) {
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
+
+  delete objeto[unaPropiedad]; //Para eliminar una propiedad, utilizamos **DELETE**, el parametro debe ir dentro de []
+  return objeto;
 }
 
 function nuevoUsuario (nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
-
+var usuario = {
+  nombre: nombre,
+  email: email,
+  password: password
+};
+return usuario;
 }
 
 function tieneEmail (usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+
+/* NOTA:
+Syntaxis del **IF** con **OBJETOS**
+
+if (nombre del parametro['Se coloca entre comillas la propiedad']){
+
+}
+*/
+
+//En este caso, el **IF** lo que nos dice es que 
+//**SI** el **USUARIO** tiene **EMAIL** RETORNAR TRUE
+// De lo contrario si el usuario NO tiene **EMAIL** retornar FALSE
+
+if(usuario['email']){
+  return true;
+}
+else {
+  return false;
+}
+
 }
 
 function tienePropiedad (objeto, propiedad) {
@@ -54,6 +108,16 @@ function tienePropiedad (objeto, propiedad) {
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
+
+  //if el objeto tiene propiedad, retornar true, de lo contario retornar false
+
+  if(objeto[propiedad]){
+    return true;
+  }
+  else{
+    return false;
+  }
+
 }
 
 function verificarPassword (usuario, password) {
@@ -61,12 +125,22 @@ function verificarPassword (usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // Tu código:
+
+  if(password===usuario.password){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 function actualizarPassword (usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevaPassword"
   // Devuelve el objeto
   // Tu código:
+
+
+  
 }
 
 function agregarAmigo (usuario, nuevoAmigo) {
